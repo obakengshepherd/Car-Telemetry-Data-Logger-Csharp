50dacfc09ed35e3ff446c79fa9b21de157ff1cb6
obakengshepherd <obakengtsaagane@gmail.com>
2025-07-18 09:52:43 +0200

    C#: Added sensor input prompts

---
+++ CarTelemetryLogger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarTelemetryLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Speed (km/h): ");
+            string speedInput = Console.ReadLine();
+            Console.Write("Enter RPM: ");
+            string rpmInput = Console.ReadLine();
+            Console.Write("Enter Temperature (°C): ");
+            string tempInput = Console.ReadLine();
+
+            Console.WriteLine($"You entered: Speed={speedInput}, RPM={rpmInput}, Temp={tempInput}");
+        }
+    }
+}
